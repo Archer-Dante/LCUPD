e1e4d56782f9f62a8960e9ce7c5ad274797eafb4
Archer-Dante <kirahaibane@gmail.com>
2024-02-12 13:30:57 +0300

    no message

--- v16/run-patch.bat
+++ v16/run-patch.bat
@@ -1,4 +1,6 @@
 taskkill /F /IM "Lethal Company.exe"
 del "%~dp0\BepInEx\plugins\playerbracken.bundle" /Q /S
 del "%~dp0\BepInEx\plugins\PlayerBracken.dll" /Q /S
+del "%~dp0\BepInEx\plugins\gamblingmachinebundle" /Q /S
+del "%~dp0\BepInEx\plugins\GamblersMod.dll" /Q /S
 robocopy "%~dp0\temp" "%~dp0\" /E /MOVE
