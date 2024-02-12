f28363573138986c72758ca9a2b2d6bd5997d06a
Archer-Dante <kirahaibane@gmail.com>
2024-02-12 15:19:48 +0300

    v16

--- v16/run-patch.bat
+++ v16/run-patch.bat
@@ -3,5 +3,6 @@ del "%~dp0\BepInEx\plugins\playerbracken.bundle" /Q /S
 del "%~dp0\BepInEx\plugins\PlayerBracken.dll" /Q /S
 del "%~dp0\BepInEx\plugins\gamblingmachinebundle" /Q /S
 del "%~dp0\BepInEx\plugins\GamblersMod.dll" /Q /S
+del "%~dp0\package\LethalExpansionCoreMoons\starlancermoons.lem" /Q /S
 robocopy "%~dp0\temp" "%~dp0\" /E /MOVE
 rmdir "%~dp0\temp" /Q /S
