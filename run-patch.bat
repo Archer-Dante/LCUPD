0486e6bdfe6ed127d1804cd23d3c2ee8ec22e9bf
Archer-Dante <kirahaibane@gmail.com>
2024-02-12 12:48:53 +0300

    no message

---
+++ run-patch.bat
@@ -0,0 +1,4 @@
+taskkill /F /IM "Lethal Company.exe"
+del "%~dp0\BepInEx\plugins\playerbracken.bundle" /Q /S
+del "%~dp0\BepInEx\plugins\PlayerBracken.dll" /Q /S
+robocopy "%~dp0\temp" "%~dp0\" /E /MOVE
